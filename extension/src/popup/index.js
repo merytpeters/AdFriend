ef383af20c4def65935b938638f7b96adf84e767
merytpeters <merytpeters@gmail.com>
2025-02-04 08:55:55 +0100

    Directory setup

---
+++ extension/src/popup/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Popup from "./App";
+import "./styles.css";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<Popup />);
